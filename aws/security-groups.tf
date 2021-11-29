45f54d9b1aa80eca93f9cde2584d736853f2664b
Daniel Fitzgerald <daniel.fitzgerald@lacework.net>
2021-11-29 08:53:32 +0000

    added new ingress rule for http traffic on port 80

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -17,6 +17,17 @@ resource "aws_security_group" "allow_traffic" {
     prefix_list_ids  = []
     security_groups  = []
     self             = false
+  },
+  {
+    description      = "HTTP ingress from 0.0.0.0/0"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
   }]
 
   egress = [{
